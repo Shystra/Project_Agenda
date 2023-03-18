be4f4dd397ea5d0cc3745250366c93fcac859e7f
Shystra <jlsp.2019@hotmail.com>
2023-03-18 14:45:33 -0300

    Parametros base

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+# Para criar um usuario na area admin, executar o seguinte comando:
+# ------- python manage.py createsuperuser --------------
+
+from .models import Categoria, Contato
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email',
+                    'data_criacao', 'categoria') # Mostra principais elementos
+    
+    list_display_links = ('id', 'nome') # Deixa clicar no id/ nome
+
+    # list_filter = ('nome', 'sobrenome')
+
+    list_per_page = 10 # Limita mostrar 10 elementos por pagina
+
+    search_fields = ('nome', 'sobrenome', 'telefone') # Cria-se um campo de pesquisa
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
+
+
--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.utils import timezone   
 
-# Create your models here.
+
+
+# --------- A cada alteração realizada nas chaves, será necessário executar
+# O comando python manage.py makemigrations e logo depois o python manage.py migrate
+class Categoria (models.Model):
+    nome = models.CharField (max_length = 255)
+
+    def __str__(self):
+        return self.nome
+class Contato (models.Model):
+    nome = models.CharField (max_length = 255)
+    sobrenome = models.CharField (max_length = 255, blank = True)
+    telefone = models.CharField (max_length = 255)
+    email = models.CharField (max_length = 255, blank = True)
+
+    data_criacao = models.DateTimeField (default = timezone.now)
+    descricao = models.TextField (blank = True)
+    
+    categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+
+    # Função usada para chamar o id nome para a pagina incial
+    def __str__(self):
+        return self.nome
+
+
+"""
+CONTATOS
+id: INT (automático)
+
+nome: STR * (obrigatorio)
+sobrenome: STR * (opcional)
+telefone: STR * (obrigatorio)
+e-mail: STR (opcional)
+
+data_criacao: DATETIME (automático)
+descrição: texto
+categoria: CATEGORIA (outro model)
+
+CATEGORIA
+id: INT
+nome: STR * (obrigatorio)
+
+"""
+
