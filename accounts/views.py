d176f1d95e58e79ea7b4d8d2d8adb34f1e87b2de
Shystra <jlsp.2019@hotmail.com>
2023-03-20 17:03:17 -0300

    Subindo app Accounts

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def login (request):
+    return render (request, 'accounts/login.html')
+
+def logout (request):
+    return render (request, 'accotuns/logout.html')
+
+def cadastro (request):
+    return render (request, 'accounts/cadastro.html')
+
+def dashboard (request):
+    return render (request, 'accounts/dashboard.html')
+
